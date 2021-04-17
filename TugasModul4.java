ba84d13de0954245285ad6d89d74dec63c6d0e8b
Fitchreid <asroruddinahmad09@gmail.com>
2021-04-17 23:06:07 +0700

    Ahmad Asroruddin, function kamar tersedia

--- TugasModul4.java
+++ TugasModul4.java
@@ -1,4 +1,13 @@
 package com.example;
 
 public class TugasModul4 {
+    static void KamarTersedia(String[][]ruang){
+        for(int i = 0; i<=3; i++){
+            if(ruang[i][1].equals("kosong")){
+                String[] available = new String[4];
+                available[i] = ruang[i][0];
+                System.out.println(available[i]);
+            }
+        }
+    }
 }
